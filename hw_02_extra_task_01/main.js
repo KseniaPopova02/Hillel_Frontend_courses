cb0ddc1d4e628b7f2e2ff4fa4f26dabff6da1a6d
Ksenia Popova <kseniapopova02@gmail.com>
2022-08-04 12:46:05 +0200

    extra task #1

---
+++ hw_02_extra_task_01/main.js
@@ -0,0 +1,15 @@
+let letter = prompt("Введите букву");
+
+switch (letter) {
+    case "a":
+        alert(1);
+        break;
+    case "b":
+        alert(2);
+        break;
+    case "c":
+        alert(3);
+        break;
+    default: 
+    alert("Нет таких значений")
+}
