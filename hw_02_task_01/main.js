ddf41856a42268c6540b9533c2a8b1969d44e31b
Ksenia Popova <kseniapopova02@gmail.com>
2022-08-04 11:57:41 +0200

    Compleated 2 tasks

--- hw_02_task_01/main.js
+++ hw_02_task_01/main.js
@@ -1,4 +1,32 @@
 const inputNum = prompt("Enter your 3-digit number:");
-const midNum = inputNum.split("").join(" ");
+const midNum = inputNum.split("");
 
+if (midNum.length == 3 && !isNaN(inputNum)) {
+    console.log(midNum)
+    let counts = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0,
+        9: 0
+    }
+    for (let i in midNum) {
+        counts[midNum[i]]++;
+    }
+    let alertShown = false;
+     for(let i in counts) {
+        if (counts[i] > 1) {
+            alert (`The number ${i} is repeated ${counts[i]} times`)
+            alertShown = true;
+        }
+     } 
+     if(!alertShown){
+        alert("No repeating numbers found")
+     }
+}
 
--- hw_02_task_02/main.js
+++ hw_02_task_02/main.js
@@ -1,5 +1,15 @@
 const num = prompt("Enter your 6-digit number");
 
-if(num.length == 6 && isNaN(num)) {
-    
+if(num.length == 6 && !isNaN(num)) {
+    let firstPart = num.slice(0, 3);
+    let lastPart = num.slice(3, 6);
+    console.log(firstPart);
+    console.log(lastPart);
+    let lastPartReversed = lastPart.split("").reverse().join("");
+    console.log(lastPartReversed);
+    if(firstPart == lastPartReversed) {
+        alert("This numbers are mirrored")
+    }else {
+        alert("This numbers aren't mirrored")
+    }
 }
