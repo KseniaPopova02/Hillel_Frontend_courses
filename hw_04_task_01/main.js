385aa5e2f89598714d2b97aa271d7fa33f213a18
Ksenia Popova <kseniapopova02@gmail.com>
2022-08-12 11:28:58 +0200

    fourth hw

---
+++ hw_04_task_01/main.js
@@ -0,0 +1,41 @@
+let numArr = [];
+let needsPrompt = true;
+let num;
+
+while(needsPrompt){
+    num = prompt("Enter your number:");
+    
+    if (num && !isNaN(num)) {
+        numArr.push(num)
+    }else {
+        needsPrompt = false;
+        alert("We are done!");
+    }
+};
+
+console.log(numArr);
+
+//Sorting function 
+
+function compareNumeric(a, b) {
+    return a - b;
+};
+
+console.log(numArr.sort(compareNumeric));
+
+/*
+Or without creating a function
+console.log(newArr.sort((a, b) => a - b));
+*/
+
+//Deleting elements
+
+const pos = 2, n = 2;
+
+console.log(numArr.splice(pos, n));
+
+
+
+
+
+
