8f92d3c803f9e25ef6f3a3687a0934289750c8a6
Ksenia Popova <kseniapopova02@gmail.com>
2022-08-19 11:13:48 +0200

    added comments

--- hw_03_task_04/main.js
+++ hw_03_task_04/main.js
@@ -1,40 +1,34 @@
+//This task was hard for me, so I was commenting every action that i did, for better understanding and also to practice it words.
 
- function returnPrime () {
-    const num = prompt("Enter your integer number");
+const num = prompt("Enter your integer number");
 
-    // Check if something was entered in the input
-    if (!num) {
-        // If nothing was typed, we stop the script
-        return;
-    }
-
+// Check if something was entered in the input
+if (num) {
     // Check if the number is not an integer
     if (num % 1 !== 0) {
         // Show an alert to the user
         alert('This number is not an integer!');
+    } else {
+        // Setup a variable to check the prime number (we will use this variable to check if the number is prime)
+        let isPrime = true;
 
-        // Stop everything we are doing, because the number is not an integer
-        return;
-    }
+        // Loop through all the numbers from 2 to the number we entered
+        for (let i = 2; i < num; i++) {
+            // Check if the number is divisible by the number we are checking
+            if (num % i === 0) {
+                // If it is divisible, the number is not prime
+                isPrime = false;
+            }
+        }
 
-    // Setup a variable to check the prime number (we will use this variable to check if the number is prime)
-    let isPrime = true;
-
-    // Loop through all the numbers from 2 to the number we entered
-    for (let i = 2; i < num; i++) {
-        // Check if the number is divisible by the number we are checking
-        if (num % i === 0) {
-            // If it is divisible, the number is not prime
-            isPrime = false;
+        // Check if the number was prime
+        if (isPrime) {
+            // This number is prime, show an alert to the user
+            alert("This is a prime number")
+        } else {
+            // This number is not prime, show an alert to the user
+            alert("This number is not prime")
         }
     }
+}
 
-    // Check if the number was prime
-    if (isPrime) {
-        // This number is prime, show an alert to the user
-        alert("This is a prime number")
-    } else {
-        // This number is not prime, show an alert to the user
-        alert("This number is not prime")
-    }
-};
