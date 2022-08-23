d97a6a3b141b36827d69cd09d1cfcc9e89f66fdb
Ksenia Popova <kseniapopova02@gmail.com>
2022-08-23 13:25:07 +0200

    homework #7

--- hw_07_task_01/main.js
+++ hw_07_task_01/main.js
@@ -7,39 +7,22 @@ if (age < 18) {
     function User(name, age) {
         this.name = name;
         this.age = age;
-        this.returnAgeName = function () {
-            console.log("Name:", this.name)
-            console.log("Age:", this.age)
-        }
     }
 
     const someone = new User(name, age);
-
-    console.log(someone)
-
-    someone.returnAgeName();
 
     const autoBrand = prompt("Brand:");
     const autoColor = prompt("Color:");
     const autoYear = prompt("Year:");
 
-    function Auto (brand, color, year) {
+    function Auto (brand, color, year, owner) {
         this.brand = brand;
         this.color = color;
         this.year = year;
-        this.setOwner = function (user) {
-            this.owner = user;
-            console.log(`This is a car owner. Name: ${this.owner.name} Age: ${this.owner.age}`)
-        }
-        this.returnBrandColorYear = function () {
-            console.log("Brand: ", brand);
-            console.log("Color: ", color);
-            console.log("Year: ", year);
-        }
+        this.owner = owner;
     }
 
-    const auto = new Auto (autoBrand, autoColor, autoYear);
+    const auto = new Auto (autoBrand, autoColor, autoYear, someone);
    
-    auto.setOwner(someone);
-    auto.returnBrandColorYear();
+   console.log(auto)
 }
